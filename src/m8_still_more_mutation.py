38f4bdf755e30b4e93276637ae4444d87fc75855
smileyle <smileyle@rose-hulman.edu>
2018-10-17 11:10:15 -0400

    All good!

--- src/m8_still_more_mutation.py
+++ src/m8_still_more_mutation.py
@@ -4,8 +4,8 @@ In this module, you mutate by DELETING elements of a list.
 
 Authors: David Mutchler, Amanda Stouder, Chandan Rupakheti, Katie Dion,
          Claude Anderson, Delvin Defoe, Curt Clifton, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Lauren Smiley.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import m6_mutation
 
@@ -52,6 +52,11 @@ def run_test_RETURN_delete_negatives():
 
 
 def RETURN_delete_negatives(numbers):
+    newt = []
+    for k in range(len(numbers)):
+        if numbers[k] >= 0:
+            newt = newt + [numbers[k]]
+    return newt
     """
     Returns a NEW list that is the same as the given list of numbers,
     but with each negative number in the list DELETED from the list.
@@ -65,7 +70,7 @@ def RETURN_delete_negatives(numbers):
       :type numbers: list
     where the list is a list of numbers.
     """
-    # TODO: 2. First, READ THE ABOVE TEST CODE.
+    # Done: 2. First, READ THE ABOVE TEST CODE.
     #          Make sure that you understand it.
     #          In particular, note how it calls the   run_test   function
     #          from the module   m6_mutation   by using the notation:
@@ -111,6 +116,9 @@ def run_test_MUTATE_delete_negatives():
 
 
 def MUTATE_delete_negatives(numbers):
+    for k in range(len(numbers)-1, -1, -1):
+        if numbers[k] < 0:
+            del numbers[k]
     """
     MUTATES the given list of numbers so that each negative number
     in the list is DELETED from the list.
@@ -124,7 +132,7 @@ def MUTATE_delete_negatives(numbers):
 
     Precondition: The argument is a list of numbers.
     """
-    # TODO: 3. First, READ THE ABOVE TEST CODE.
+    # Done: 3. First, READ THE ABOVE TEST CODE.
     #          Make sure that you understand it.
     #          In particular, note how it calls the   run_test   function
     #          from the module   m6_mutation   by using the notation:
