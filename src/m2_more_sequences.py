1aa2cc5d800e206e95ae3835996d0e864aa6a927
smileyle <smileyle@rose-hulman.edu>
2018-10-22 10:52:13 -0400

    All good!

--- src/m2_more_sequences.py
+++ src/m2_more_sequences.py
@@ -10,8 +10,8 @@ for ITERATING through SEQUENCES, including selections from:
   -- Looking at two sequences in parallel
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Lauren Smiley.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -78,6 +78,19 @@ def run_test_shortest_string():
 
 
 def shortest_string(strings):
+    length = len(strings[0])
+    string = ''
+    count = 0
+    for k in range(len(strings)):
+        if len(strings[k]) < length:
+            length = len(strings[k])
+            string = strings[k]
+            count = count + 1
+    if count == 0:
+        return strings[0]
+    return string
+
+
     """
     What comes in:
       -- a non-empty sequence of strings
@@ -103,7 +116,7 @@ def shortest_string(strings):
       :type strings: list[str]   or tuple(str)
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DOne: 2. Implement and test this function.
     #     The testing code is already written for you (above).
     # ------------------------------------------------------------------
 
@@ -149,6 +162,14 @@ def run_test_index_of_largest_number():
 
 
 def index_of_largest_number(numbers, n):
+    value = numbers[0]
+    index = 0
+    for k in range(n):
+        if numbers[k] > value:
+            value = numbers[k]
+            index = k
+
+    return index
     """
     What comes in:
       -- a sequence of numbers
@@ -180,7 +201,7 @@ def index_of_largest_number(numbers, n):
 
     """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DOne: 3. Implement and test this function.
     #     The testing code is already written for you (above).
     # ------------------------------------------------------------------
 
@@ -218,6 +239,19 @@ def run_test_number_of_stutters():
 
 
 def number_of_stutters(s):
+    letters = s[0]
+    count = 0
+    check = 0
+    for k in range(len(s)):
+        if s[k] == letters:
+            check = check + 1
+            if check == 2:
+                count = count + 1
+                check = 1
+        letters = s[k]
+
+    return count
+
     """
     What comes in:
       -- a string s
@@ -235,7 +269,7 @@ def number_of_stutters(s):
        :type s: str
     """
     # ------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # Done: 4. Implement and test this function.
     #     The testing code is already written for you (above).
     # ------------------------------------------------------------------
 
@@ -280,6 +314,10 @@ def run_test_is_palindrome():
 
 
 def is_palindrome(s):
+    for k in range(len(s)//2):
+        if s[k] != s[(len(s)-k)-1]:
+            return False
+    return True
     """
     What comes in:
       -- a string s that (in this simple version of the palindrome
@@ -308,7 +346,7 @@ def is_palindrome(s):
       :type s: str
     """
     # ------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # Done: 5. Implement and test this function.
     #     The testing code is already written for you (above).
     #
     ####################################################################
@@ -347,6 +385,11 @@ def run_test_count_same():
 
 
 def count_same(sequence1, sequence2):
+    count = 0
+    for k in range(len(sequence1)):
+        if sequence1[k] == sequence2[k]:
+            count = count + 1
+    return count
     """
     What comes in:
       -- two sequences that have the same length
@@ -375,7 +418,7 @@ def count_same(sequence1, sequence2):
       type: sequence2: tuple or list or string
     """
     # ------------------------------------------------------------------
-    # TODO: 6. Implement and test this function.
+    # Done: 6. Implement and test this function.
     #     The testing code is already written for you (above).
     # ------------------------------------------------------------------
 
